e8bd1c563e6dd4b1e46b8028cf766129604baa83
manishkhanal9841 <manishkhanal@gmail.com>
2019-11-11 21:29:14 +0545

    Trying new

---
+++ manishfile.php
@@ -0,0 +1,30 @@
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+	<tbody>
+		<tr>
+			<td valign="top" width="170">
+				<div align="center">
+					<img alt="rekha" height="150" src="/assets/images/team/rekha.jpg" width="150" /><br />
+					<strong>Rekha Kumari Bhandari</strong></div>
+			</td>
+			<td valign="top">
+				<p>
+					Ms. Bhandari has served CMF Nepal since February 2012 as a Project Officer under the IRHM Project. Before joining CMF, Ms. Bhandari worked at various non-profit organizations&nbsp;including the Women Awareness Center Nepal (WACN), ABC Nepal, and the National Network Against Girl Trafficking (NNAGT). Ms. Bandari has 15 years of experience in the microfinance sector.</p>
+				<div>
+				  <p><strong><span lang="EN-US">STRENGTHS AND SKILLS</span></strong></p>
+				  <div>
+				    <p><strong><span lang="EN-US">Tools</span></strong><span lang="EN-US">: Familiar in institutional Audit, Client Protection Principles Assessment tools, PEARLS Monitoring tool</span></p>
+				    <p><strong><span lang="EN-US">Project design</span></strong><span lang="EN-US">: </span></p>
+				    <p><strong><span lang="EN-US">Project management</span></strong><span lang="EN-US">: planning, implementation, social mobilization, monitoring, supervision, Evaluation, reporting </span></p>
+				    <p><strong><span lang="EN-US">Analysis</span></strong><span lang="EN-US">: financial performance of cooperatives</span></p>
+				    <p><strong><span lang="EN-US">Training delivery</span></strong><span lang="EN-US">: Financial Education, groups and cooperatives management; Cooperative Accounting, portfolio management, risk and delinquency management for Cooperatives </span></p>
+				    <p><strong><span lang="EN-US">Documentation</span></strong><span lang="EN-US">: report writing, case writing </span></p>
+				    <p><strong><span lang="EN-US">Advocacy</span></strong><span lang="EN-US">: Presentations, lobbying and advocacy; </span></p>
+				    <p><strong><span lang="EN-US">Computer</span></strong><span lang="EN-US">: Excellent in word, excel, PowerPoint, email, Skype, and internet</span></p>
+				    <p><strong><span lang="EN-US">Exposures</span></strong><span lang="EN-US">: Professional visits to Bangladesh, Cambodia and about 50 districts of Nepal</span></p>
+			      </div>
+			  </div>
+				
+			</td>
+		</tr>
+	</tbody>
+</table>
